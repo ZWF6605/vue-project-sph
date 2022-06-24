b4ac1797efbf7cbbd8467ef9309df8b1312e90a7
unknown <605944384@qq.com>
2022-06-24 13:38:54 +0800

    引入Vuex进行模块式开发

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import router from '@/router'
 import TypeNav from '@/pages/Home/TypeNav'
 //第一个参数：全局组件的name 第二个参数：对应的组件
 Vue.component(TypeNav.name,TypeNav)
-
-
+//引入仓库
+import store from '@/store'
+//测试接口
 import {reqCategoryList} from '@/api'
 reqCategoryList()
 
@@ -20,4 +21,6 @@ new Vue({
   //注册路由：以下写法是kv一致省略v【router是小写的】
   //注册路由信息：当这里书写router的时候，组件身上都拥有$route $router属性
   router,
+  //注册仓库：组件实例的身上会多一个$store属性
+  store
 }).$mount('#app')
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -9,21 +9,23 @@
     <Floor></Floor>
     <Floor></Floor>
     <Brand></Brand>
+    
   </div>
 </template>
 
 <script>
-import ListContainer from '@/pages/Home/ListContainer'
-import Recommend from '@/pages/Home/Recommend'
-import Rank from '@/pages/Home/Rank'
-import Like from '@/pages/Home/Like'
-import Floor from '@/pages/Home/Floor'
-import Brand from '@/pages/Home/Brand'
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+
 export default {
-  components:{ListContainer,Recommend,Rank,Like,Floor,Brand}
-}
+  components: { ListContainer, Recommend, Rank, Like, Floor, Brand },
+  
+};
 </script>
 
 <style>
-
 </style>
